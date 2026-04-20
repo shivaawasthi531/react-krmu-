8683d74d00c41b3bc801aee453db46fd68c20fc9
Shiva Awasthi <shivaawasthi531@gmail.com>
2026-04-20 14:22:23 +0530

    Update Sample.jsx

--- src/components/Sample.jsx
+++ src/components/Sample.jsx
@@ -1,30 +1,68 @@
-import React,{Component, useEffect, useState} from 'react'
+
+import React,{Component, useState, useEffect, useRef} from 'react'
 import style from '../css/Sample.module.css'
 import style1 from '../css/HomePage.module.css'
 // import styled from 'styled-components'
 import styled from '@emotion/styled'
 import { css } from '@emotion/react'
+import axios from 'axios'
 
 function Sample(props) {
     const [name,setName]=useState("Devendra")
     const [age,setAge]=useState(20)
+    const [loading, setLoading] = useState(false)
+    const inputRef=useRef(null)
 
     useEffect(()=>{
+        
         console.log("Component mounted")
         return ()=>{
             console.log("Component unmounted")
         }
-    }, [name])
- 
+    },[name])
+
+    useEffect(()=>{
+        // async function fetchingData(){
+        //     const response=await fetch("https://dummyjson.com/products",{
+        //         method:"GET"
+        //     })
+        //     const data=await response.json()
+        //     console.log(data)
+        // }
+
+        async function fetchingData(){
+            try{
+                setLoading(true)
+                const response=await axios.get("https://dummyjson.com/products/")
+            console.log(response.data)
+            setLoading(false)
+            }catch(error){
+                console.log(error)
+            }
+            }
+        
+        fetchingData()
+
+        // inputRef.current.value="Hello"
+        // inputRef.current.focus()
+        },[])
+    const handleChange=(e)=>{
+        setName(e.target.value)
+    }
+
   return (
     <div>
+        {loading?<h1>Loading...</h1>:<h1></h1>}
       <h2>Trending topics</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quae maiores, nobis laboriosam non commodi quia consequuntur vitae dolore facere deserunt quas recusandae iste harum in, provident aut reprehenderit temporibus.</p>
-      <h3>{name}</h3>
+      <h1>{name}</h1>
         <button onClick={()=>{
             setName("Alex")
             
         }}>Name Change</button>
+
+        {/* <input type='text' placeholder='"Enter the text' ref={inputRef}></input> */}
+        <input type='text' placeholder='"Enter the text' onChange={handleChange}></input>
     <button
         className="counter"
         onClick={() => props.setCount((count) => count + 1)}
